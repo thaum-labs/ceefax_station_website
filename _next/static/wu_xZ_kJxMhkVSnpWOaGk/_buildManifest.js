edc714decd93ad5cec8768c03b04c38cf1a7167c
THAUM-io-commits <73592328+THAUM-io-commits@users.noreply.github.com>
2025-12-29 18:43:38 +0000

    Remove old non-Ceefax project files (thaum website assets) from index.html branch

--- _next/static/wu_xZ_kJxMhkVSnpWOaGk/_buildManifest.js
+++
@@ -1,7 +0,0 @@
-self.__BUILD_MANIFEST = {
-    __rewrites: [],
-    "/": ["static\u002Fchunks\u002Fpages\u002Findex-bae5efd36713df545b7a.js"],
-    "/_error": ["static\u002Fchunks\u002Fpages\u002F_error-17df1be5c246667e07ff.js"],
-    sortedPages: ["\u002F", "\u002F_app", "\u002F_error"]
-};
-self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
